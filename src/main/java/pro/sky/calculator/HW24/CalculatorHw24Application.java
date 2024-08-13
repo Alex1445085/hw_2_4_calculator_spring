6a863c0d94d8bde3479441b48d8654c4ce2eae0f
Alex1445085 <n1445085@yandex.ru>
2024-08-13 23:35:35 +0300

    new commit

---
+++ src/main/java/pro/sky/calculator/HW24/CalculatorHw24Application.java
@@ -0,0 +1,12 @@
+package pro.sky.calculator.HW24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHw24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorHw24Application.class, args);
+	}
+}
